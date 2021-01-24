b8420b8f9f34ae0ef2e94d5339f1b83b412114dc
albertoribeiro <alberto.r.flavio@gmail.com>
2021-01-24 12:17:35 -0300

    desafio fundamentos React-native

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,13 @@
 import Transaction from '../models/Transaction';
+import transactionRouter from '../routes/transaction.routes';
+
+
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 
 interface Balance {
   income: number;
@@ -14,15 +23,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    
+    const balance = this.transactions.reduce(
+      (accumulator: Balance,transaction:Transaction) => {
+      switch( transaction.type){
+        case "income":
+          accumulator.income += transaction.value;
+          break;
+        case "outcome":
+          accumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+      return accumulator;
+    },{
+      income: 0,
+      outcome:0,
+      total: 0,
+    })
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}:CreateTransactionDTO): Transaction {
+    
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
